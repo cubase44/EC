3c50a48996e18cc07a86884c7eebb7031700d5c3
{cubase44} <{cubase44unlock333@gmail.com}>
2022-04-13 14:19:55 +0900

    トップページの表示修正

--- src/resources/views/layouts/navigation.blade.php
+++ src/resources/views/layouts/navigation.blade.php
@@ -93,7 +93,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-white">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navigator</a>
+    <a class="navbar-brand" href="/dashboard">Navigator</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -112,7 +112,7 @@
           <a class="nav-link active" aria-current="page" href="#">顧客管理</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">設定</a>
+          <a class="nav-link active" aria-current="page" href="/user">設定</a>
         </li>
       </ul>
     </div>
